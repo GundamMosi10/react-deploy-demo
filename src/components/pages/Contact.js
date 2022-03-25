3bf3f16462003e1d660d60138fe978c1cae4f81b
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-24 20:01:26 -0400

    added the css file for the contact me page and the page itself

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,67 @@
 import React from 'react';
-
+import '../styles/contact.css';
 
 export default function Contact() {
   return (
-    <div>
+    <div className="container">
       <h1>Connect With Me</h1>
+      <div className="contact-box">
+        <div className="contact-left">
+          <h3>Send your Request</h3>
+          <form>
+
+            <div className="input-row">
+              <div className="input-group">
+                <label>Name</label>
+                <input type="text" placeholder="John Smith"></input>
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input-group">
+                <label>Phone</label>
+                <input type="text" placeholder="1 212 345 6789"></input>
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input-group">
+                <label>Email</label>
+                <input type="email" placeholder="johnsmith@gmail.com"></input>
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input-group">
+                <label>Subject</label>
+                <input type="text" placeholder="placeholder"></input>
+              </div>
+            </div>
+            <label>Message</label>
+            <textarea rows="5" placeholder="Your Message"></textarea>
+            <button type="submit">SEND</button>
+
+          </form>
+
+        </div>
+
+
+
+        <div className="contact-right">
+          <h3>Reach Me</h3>
+          <table>
+            <tr>
+              <td>Email</td>
+              <td>contactme@example.com</td>
+            </tr>
+            <tr>
+              <td>Phone</td>
+              <td>1 212 345 6789</td>
+            </tr>
+            <tr>
+              <td>Address</td>
+              <td>20 W 34th St, New York, NY 10001</td>
+            </tr>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
